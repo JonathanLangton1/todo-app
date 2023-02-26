b92d178c422340f8b2b967249277c4275fc04a5c
Jonathan <jonathanlangton3@gmail.com>
2023-02-26 17:26:49 +0000

    Added non-functional today & yesterday tasks UI final

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col bg-slate-100 p-8 text-g text-slate-700">
         <h1 className="text-3xl font-medium mb-16" onClick={generateUniqueListId}>Your Todos</h1>
 
-        <section className="mb-4">
+        <section className="mb-2">
           <h2 className="text-xs font-medium mb-2">Your lists</h2>
           <div className="flex gap-2 overflow-x-auto snap-x snap-mandatory">
             {lists.map(list => <ListSummary listName={list.listName} noOfTask={list.tasks.length} key={list.id} themeColour={list.themeColour} />)}
@@ -47,32 +47,69 @@ const Home: NextPage = () => {
           </div>
         </section>
 
-        <section className="mb-4">
-          <h3 className="text-xs font-medium mb-2">Today</h3>
-          <div className="flex flex-col gap-2">
+        <section className="mb-4 overflow-y-auto overflow-x-visible h-[calc(100svh-15.75rem)]">
+          <div>
+            <h3 className="text-xs font-medium py-2 sticky top-0 bg-slate-100 z-[2]">Today</h3>
+            <div className="flex flex-col gap-2">
 
-            <div className="flex bg-white p-3 rounded-lg gap-2 items-center">
-              <input type="checkbox" name="task-checkbox" id="task-checkbox" className="absolute opacity-0" />
-              <div className="flex justify-center items-center">
-                <label htmlFor="task-checkbox" className="w-4 h-4 border rounded-full border-slate-400 cursor-pointer z-10"></label>
+              <div className="flex bg-white p-3 rounded-lg gap-2 items-center">
+                <input type="checkbox" name="task-checkbox" id="task-checkbox" className="absolute opacity-0" />
+                <div className="flex justify-center items-center">
+                  <label htmlFor="task-checkbox" className="w-4 h-4 border rounded-full border-slate-400 cursor-pointer z-[1]"></label>
+                </div>
+                <p className="font-medium">Example task</p>
+                <div className="w-[0.75rem] h-[0.75rem] rounded bg-black ml-auto"></div>
               </div>
-              <p className="font-medium">Example task</p>
-              <div className="w-[0.75rem] h-[0.75rem] rounded bg-black ml-auto"></div>
+
+
+              {Array.from({ length: 10 }, (_, i) => {
+              return (<div key={i} className="flex bg-white p-3 rounded-lg gap-2 items-center opacity-50">
+                <input type="checkbox" name="task-checkbox" id="task-checkbox" className="absolute opacity-0" />
+                <div className="flex justify-center items-center">
+                  <label htmlFor="task-checkbox" className="w-4 h-4 border rounded-full border-slate-400 bg-slate-400 cursor-pointer z-[1]"></label>
+                  <Check className="absolute w-3 text-white z-20" />
+                </div>
+                <p className="font-medium line-through">Example task</p>
+                <div className="w-[0.75rem] h-[0.75rem] rounded bg-pink-500 ml-auto"></div>
+              </div>)
+              })}
+
             </div>
 
-            <div className="flex bg-white p-3 rounded-lg gap-2 items-center opacity-50">
-              <input type="checkbox" name="task-checkbox" id="task-checkbox" className="absolute opacity-0" />
-              <div className="flex justify-center items-center">
-                <label htmlFor="task-checkbox" className="w-4 h-4 border rounded-full border-slate-400 bg-slate-400 cursor-pointer z-10"></label>
-                <Check className="absolute w-3 text-white z-20" />
+
+
+            <h3 className="text-xs font-medium py-2 sticky top-0 bg-slate-100 z-[2]">Yesterday</h3>
+            <div className="flex flex-col gap-2">
+
+              <div className="flex bg-white p-3 rounded-lg gap-2 items-center">
+                <input type="checkbox" name="task-checkbox" id="task-checkbox" className="absolute opacity-0" />
+                <div className="flex justify-center items-center">
+                  <label htmlFor="task-checkbox" className="w-4 h-4 border rounded-full border-slate-400 cursor-pointer z-[1]"></label>
+                </div>
+                <p className="font-medium">Example task</p>
+                <div className="w-[0.75rem] h-[0.75rem] rounded bg-black ml-auto"></div>
               </div>
-              <p className="font-medium line-through">Example task</p>
-              <div className="w-[0.75rem] h-[0.75rem] rounded bg-pink-500 ml-auto"></div>
-            </div>
+
+
+              {Array.from({ length: 10 }, (_, i) => {
+               return (<div key={i} className="flex bg-white p-3 rounded-lg gap-2 items-center opacity-50">
+                <input type="checkbox" name="task-checkbox" id="task-checkbox" className="absolute opacity-0" />
+                <div className="flex justify-center items-center">
+                  <label htmlFor="task-checkbox" className="w-4 h-4 border rounded-full border-slate-400 bg-slate-400 cursor-pointer z-[1]"></label>
+                  <Check className="absolute w-3 text-white z-20" />
+                </div>
+                <p className="font-medium line-through">Example task</p>
+                <div className="w-[0.75rem] h-[0.75rem] rounded bg-pink-500 ml-auto"></div>
+              </div>)
+              })}
+
+          </div>
+
 
 
           </div>
         </section>
+
       </main>
     </>
   );
